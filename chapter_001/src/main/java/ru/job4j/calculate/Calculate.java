f55509f57b8f67fc7846c6308f826931fb77c09c
IliasAI <jaguarth@mail.ru>
2019-04-21 17:30:42 +0500

    1.3. JavaDoc. Code convention[#135631]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,14 @@
 package ru.job4j.calculate;
-
+/**
+*Class Calculate решение задачи 001 урок 1.
+*@author Ilias
+*@since 21.04.2019
+*/
 public class Calculate {
-	
+	/**
+	*Точка входа в класс
+	*@param args - массив строковых аргументов
+	*/	
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ilias Agakhanov (ferr0rcode@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
