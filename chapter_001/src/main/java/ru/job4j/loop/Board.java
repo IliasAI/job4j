2fa02513414e45abc393b681a852e0d85b02a463
IliasAI <jaguarth@mail.ru>
2019-04-27 01:31:26 +0500

    5.3. Построить шахматную доску в псевдографике.[#135603]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+/**
+ * @author Ilias Agakhanov (ferr0rcode@gmail.com)
+ * @version 1.0
+ */
+public class Board {
+    /**
+     * Метод рисует доску заданных размеров.
+     * @param width ширина доски.
+     * @param height высота доски.
+     * @return доска.
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if ((i + j) % 2 == 0) {
+                    screen.append("x");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Ilias Agakhanov
+ */
+public class BoardTest {
+    /**
+     * Test board 3x3.
+     */
+    @Test
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x%s x %sx x%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test board 5x4.
+     */
+    @Test
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        //напишите здесь тест, проверяющий формирование доски 5 на 4.
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line);
+        assertThat(result, is(expected));
+    }
+}
+
