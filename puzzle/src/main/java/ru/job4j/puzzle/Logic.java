1fb34a98cd930ad52b4df90db20c4954ed00be7a
IliasAI <jaguarth@mail.ru>
2019-04-30 14:00:04 +0500

    Пазлы. Тестовое задание.[#135637]

---
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -0,0 +1,125 @@
+package ru.job4j.puzzle;
+
+import ru.job4j.puzzle.firuges.Cell;
+import ru.job4j.puzzle.firuges.Figure;
+
+/**
+ * //TODO add comments.
+ *
+ * @author Ilias Agakhanov (ferr0rcode@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Logic {
+    private final int size;
+    private final Figure[] figures;
+    private int index = 0;
+
+    public Logic(int size) {
+        this.size = size;
+        this.figures = new Figure[size * size];
+    }
+
+    public void add(Figure figure) {
+        this.figures[this.index++] = figure;
+    }
+
+    public boolean move(Cell source, Cell dest) {
+        boolean rst = false;
+        int index = this.findBy(source);
+        if (index != -1) {
+            Cell[] steps = this.figures[index].way(source, dest);
+            if (this.isFree(steps)) {
+                rst = true;
+                this.figures[index] = this.figures[index].copy(dest);
+            }
+        }
+        return rst;
+    }
+
+    public boolean isFree(Cell... cells) {
+        boolean result = cells.length > 0;
+        for (Cell cell : cells) {
+            if (this.findBy(cell) != -1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public void clean() {
+        for (int position = 0; position != this.figures.length; position++) {
+            this.figures[position] = null;
+        }
+        this.index = 0;
+    }
+
+    private int findBy(Cell cell) {
+        int rst = -1;
+        for (int index = 0; index != this.figures.length; index++) {
+            if (this.figures[index] != null && this.figures[index].position().equals(cell)) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * Метод возвращает true если по вертикали или горизонтали еденицы образуют ряд нужного размера.
+     *
+     * @return true или false.
+     */
+    public boolean isWin() {
+        int[][] table = this.convert();
+        boolean result = false;
+        int horizontal = 0;
+        int vertical = 0;
+
+        //проверка по горизонтали
+        for (int[] row : table) {
+            if (horizontal == table.length) {
+                result = true;
+                break;
+            }
+            for (int value : row) {
+                if (value == 1) {
+                    horizontal++;
+                } else {
+                    break;
+                }
+            }
+        }
+        if (!result) {
+            //проверка по вертикали
+            for (int out = 0; out < table.length; out++) {
+                if (vertical == table.length) {
+                    result = true;
+                    break;
+                }
+                for (int in = 0; in < table.length; in++) {
+                    if (table[in][out] == 1) {
+                        vertical++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public int[][] convert() {
+        int[][] table = new int[this.size][this.size];
+        for (int row = 0; row != table.length; row++) {
+            for (int cell = 0; cell != table.length; cell++) {
+                int position = this.findBy(new Cell(row, cell));
+                if (position != -1 && this.figures[position].movable()) {
+                    table[row][cell] = 1;
+                }
+            }
+        }
+        return table;
+    }
+}
